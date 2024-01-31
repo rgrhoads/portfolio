2f74bd3f5953a354fadebb44856aa3a4cfbbd0e3
ryanrhoads <rrhoads@graymatters-inc.com>
2024-01-31 15:13:52 -0500

    comment out resume section for now

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -42,14 +42,14 @@ const Sidenav = () => {
                         <AiOutlineProject size={20}/>
                         <span className='pl-4'>Projects</span>
                     </a>
-                    <a 
+                    {/* <a 
                         href="#main" 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         onClick={handleNav}
                     >
                         <BsPerson size={20}/>
                         <span className='pl-4'>Resume</span>
-                    </a>
+                    </a> */}
                     <a 
                         href="#contact" 
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
@@ -73,9 +73,9 @@ const Sidenav = () => {
                     <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineProject size={20} />
                     </a>
-                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    {/* <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <BsPerson size={20} />
-                    </a>
+                    </a> */}
                     <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineMail size={20} />
                     </a>
