cc02b0d6703caa12e8f0618bf3edf22b527fc553
ryanrhoads <rrhoads@graymatters-inc.com>
2024-01-31 14:51:50 -0500

    set in network host mode

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 docker build -t portfolio .
-docker run -d --restart unless-stopped --name portfolio -p 5173:5173 portfolio
+docker run -d --restart unless-stopped --network host --name portfolio -p 5173:5173 portfolio
