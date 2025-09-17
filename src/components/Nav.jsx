b645c994464cad5d2b83dd395b9e7745f72dacf1
rgrhoads <rgrhoads@icloud.com>
2025-09-17 12:14:55 -0400

    add spacing for navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Sidenav = () => {
   return (
     <div className="fixed top-0 left-0 right-0 z-20">
       <div className="container mx-auto px-4">
-        <nav className="flex justify-center gap-4 py-3">
+        <nav className="flex justify-center gap-12 py-6">
           <a
             href="#main"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-3 sm:p-4 cursor-pointer hover:scale-110 ease-in duration-200"
