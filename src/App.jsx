abc3750fbaea90f4f8d570c5db6a483a3316b5bb
rgrhoads <rgrhoads@icloud.com>
2025-09-17 10:54:49 -0400

    rework nav to be on top and remove collapse into hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import Sidenav from './components/Sidenav'
-import Main from './components/Main'
-import Work from './components/Work'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Work from "./components/Work";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 // import Chat from './components/Chat'
 
 function App() {
-
   return (
-      <div>
-        <Sidenav />
-        <Main />
-        <Work />
-        <Projects />
-        <Contact />
-        {/* <Chat /> */}
-      </div>
-  )
+    <div>
+      <Nav />
+      <Main />
+      <Work />
+      <Projects />
+      <Contact />
+      {/* <Chat /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
